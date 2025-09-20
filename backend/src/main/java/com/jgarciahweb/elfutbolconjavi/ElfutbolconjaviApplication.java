18a6306da3eddf64569e957abafdcdb5be164286
jgarciahweb <jgarciahweb@gmail.com>
2025-09-20 11:45:23 +0200

    Initial structure in frontend and backend

---
+++ backend/src/main/java/com/jgarciahweb/elfutbolconjavi/ElfutbolconjaviApplication.java
@@ -0,0 +1,13 @@
+package com.jgarciahweb.elfutbolconjavi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElfutbolconjaviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElfutbolconjaviApplication.class, args);
+	}
+
+}
